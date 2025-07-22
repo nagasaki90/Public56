9e5c14ab5550abc4953b414a22e9f3ff6b4063ba
nagasaki90 <nagasakiyamamoto10@gmail.com>
2025-07-22 08:02:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const array1 =['hi']
+array1.push('dad')
+array1.pop()
+array1.unshift('jk')
+array1.shift()
+console.log(array1)
+const array2 =['maoney', 122, 'funney', '818money', 122233322]
+const arraycombo =array1.concat(array2)
+console.log(arraycombo)
+
+if(arraycombo.length=6){
+console.log(`${arraycombo} is 6 items long`)
+}
+const array3 =["kl rahul"] 
+console.log(array3)
+array3.push("hi")
+array3.unshift("op")
+const array6 =["Virat Kohl"]
+console.log(array6)
+array6.push("Rohit sharma")
+array6.unshift("gamer")
+
